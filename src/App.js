06b31e3b5b5e8fad7214f28d09b00f7f3032e5b2
kowshikkuri <ratulkuri@gmail.com>
2020-08-06 20:57:14 +0600

    Upadting app with shop page, collection preview and collection item components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom'
 
 import Homepage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 import './App.css';
 
@@ -37,6 +38,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path="/" component={ Homepage } />
+        <Route exact path="/shop" component={ ShopPage } />
         <Route exact path="/shop/hats" component={ HatsPage } />
         <Route path="/shop/hats/:productId" component={ HatDetails } />
       </Switch>
