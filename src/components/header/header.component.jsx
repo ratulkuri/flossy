f376c66ad0e2eed19cbfb5b3891f1b2a694fe8ec
kowshikkuri <ratulkuri@gmail.com>
2020-08-10 23:40:22 +0600

    added dp

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,30 +5,39 @@ import { auth } from '../../firebase/firebase.utils'
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import './header.style.scss';
 
-const Header = ({currentUser}) => (
-    <div className="header">
-        <Link className='logo-container' to='/'>
-            <Logo className='logo' />
-        </Link>
-        <div className='options'>
-            <Link className='option' to='/shop'>
-                Shop
+const Header = ({currentUser}) => {
+    console.log(currentUser);
+    return (
+        <div className="header">
+            <Link className='logo-container' to='/'>
+                <Logo className='logo' />
             </Link>
-            <Link className='option' to='/contact'>
-                Contact
-            </Link>
-            {
-                currentUser ?
-                <div className='option' onClick={() => auth.signOut()}>
-                    Sign Out
-                </div>
-                :
-                <Link className='option' to='/login'>
-                    Sign In
+            <div className='options'>
+                <Link className='option' to='/shop'>
+                    Shop
                 </Link>
-            }
+                <Link className='option' to='/contact'>
+                    Contact
+                </Link>
+
+                {
+                    (currentUser && currentUser.photoURL) ?
+                    <div className="dp" style={{ backgroundImage: `url(${currentUser.photoURL})` }}></div>
+                    : null
+                }
+                {
+                    currentUser ?
+                    <div className='option' onClick={() => auth.signOut()}>
+                        Sign Out
+                    </div>
+                    :
+                    <Link className='option' to='/login'>
+                        Sign In
+                    </Link>
+                }
+            </div>
         </div>
-    </div>
-);
+    );
+}
 
 export default Header;
